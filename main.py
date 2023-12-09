5c5f7de80f118923bd025edd6232c6407f5fcbf6
Łukasz Zalewski <xlukzalx@gmail.com>
2023-12-09 21:09:10 +0100

    Added script for two communicating agents with one counting the messages

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import spade
 from spade import agent
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.message import Message
-from spade.template import Template
 
-agent1_login = ""
-agent1_password = ""
+agent1_login = "test_agent@jabb.im"
+agent1_password = "123456789"
 
-agent2_login = ""
-agent2_password = ""
+agent2_login = "test_agent_2@jabb.im"
+agent2_password = "123456789"
 
 def create_message(receiver, metadata_key, metadata_value, body):
 
@@ -18,38 +18,49 @@ def create_message(receiver, metadata_key, metadata_value, body):
 
     return msg
 
-class FirstCounterAgent(Agent):
+class SenderAgent(Agent):
 
     this_agent = agent1_login
     other_agent = agent2_login
 
     async def setup(self):
-        pass
+        
+        self.sendingBehaviour = self.SendingBehaviour()
+        self.add_behaviour(self.sendingBehaviour)
 
-class SecondCounterAgent(Agent):
+    class SendingBehaviour(CyclicBehaviour):
 
-    this_agent = agent2_login
-    other_agent = agent1_login
+        async def run(self):
+            
+            msg = create_message(agent1_login, "performative", "inform", f"Hello agent {agent1_login}")
+            await self.send(msg)
+            print(f"{agent2_login}: Sent message to {agent1_login}")
+
+class CounterAgent(Agent):
 
     async def setup(self):
-        pass
+        self.receivingBehaviour = self.ReceivingBehaviour()
+        self.add_behaviour(self.receivingBehaviour)
 
     class ReceivingBehaviour(CyclicBehaviour):
+
+        counter = 0
 
         async def run(self):
             msg = await self.receive(timeout=5)
 
             if (msg):
 
-                number = msg.body
-                number += 1
+                self.counter += 1
+                print(f"{agent1_login}: Got message from {agent2_login}. Number of counted messages: {self.counter}")
 
-                msg = create_message(self.other_agent, "performative", "inform", str(number))
+async def main():
 
-                await self.send(msg)
-                print(f"{self.this_agent}: Got {msg.body}, resent {number}")
-
+    agent_1 = CounterAgent(agent1_login, agent1_password)
+    agent_2 = SenderAgent(agent2_login, agent2_password)
+    
+    await agent_1.start()
+    await agent_2.start()
 
 if __name__ == "__main__":
-
-    pass
+    spade.run(main())
